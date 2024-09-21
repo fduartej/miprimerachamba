7b13196c977787ccb42e738d9c002aaf6ba07d9e
Fred Duarte <fduartej@gmail.com>
2024-09-21 10:01:27 -0500

    Cambio de color

--- js/behavior.js
+++ js/behavior.js
@@ -1,5 +1,11 @@
-const button = document.getElementById("buttonChange");
+const mybutton = document.getElementById("buttonChange");
 
-button.addEventListener("click", changeColor);
+mybutton.addEventListener("click", changeColor);
 
-function changeColor() {}
+function changeColor() {
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+
+  mybutton.style.backgroundcolor = "rgb(${red}, ${green}, ${blue})";
+}
